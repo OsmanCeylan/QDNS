fae83b66ef798d3fea6bcdffe9231c7d65deecb7
OsmanCeylan <forevertr3@gmail.com>
2021-07-07 06:07:24 +0300

    Requirments updated.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['QDNS', 'QDNS.tools', 'QDNS.device', 'QDNS.backend', 'QDNS.commands', 'QDNS.rtg_apps', 'QDNS.networking', 'QDNS.simulation', 'QDNS.architecture', 'tests', 'tests.new', 'tests.zero', 'tests.kernel', 'tests.unitest'],
     url='github.com/OsmanCeylan/QDNS',
     license='',
-    author='osman',
+    author='Osman Ceylan',
     author_email='osman.semi.ceylan@gmail.com',
     description='QDNS - Quantum Network Simulator on Python'
 )
