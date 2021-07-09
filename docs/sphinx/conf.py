9e092f65108527ede22ace887070ae61c2a342ea
OsmanCeylan <forevertr3@gmail.com>
2021-07-10 00:57:21 +0300

    Updated docs theme

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -13,6 +13,8 @@
 
 import os
 import sys
+import stanford_theme
+
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.append('source')
 
@@ -57,7 +59,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+
+html_theme = 'stanford_theme'
+html_theme_path = [stanford_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
