596945eb07b37aebf980afa3b068fef757673954
OsmanCeylan <forevertr3@gmail.com>
2021-07-10 23:03:49 +0300

    Updated licanse.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version=str(__version__),
     packages=['QDNS', 'QDNS.tools', 'QDNS.device', 'QDNS.backend', 'QDNS.commands', 'QDNS.rtg_apps', 'QDNS.networking', 'QDNS.simulation', 'QDNS.architecture'],
     url='github.com/OsmanCeylan/QDNS',
-    license='',
+    license='BSD',
     author='COMU Team',
     author_email='osman.semi.ceylan@gmail.com',
     description='QDNS - Quantum Network Simulator on Python'
